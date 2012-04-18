b321d385548bb7306f3a6b1586c1537614488888
Aaron Cruz <aaron@aaroncruz.com>
2012-04-18 10:10:12 +0200

    Cleaned up recipe. Removed gem update and rvm stuff

--- README.markdown
+++ README.markdown
@@ -1,2 +1,9 @@
 bootstrap files borrowed (stolen) from this tutorial  
 [Chef Solo tutorial](http://opinionatedprogrammer.com/2011/06/chef-solo-tutorial-managing-a-single-server-with-chef/#comment-910)
+
+If you are using this and you would like features added, create an issue or pull request (preferrably on a topic branch you have created in your fork)
+
+#### Todo
+
+* rbenv and ruby 1.9.3+
+* zsh, maybe with my .oh-my-zsh repo
--- cookbooks/pferdefleisch/recipes/default.rb
+++ cookbooks/pferdefleisch/recipes/default.rb
@@ -1,32 +1,25 @@
-# execute "apt-get update" do
-#   user 'root'
-# end
-# this is because my dns was messed up in my vagrant box
-# i set the dns to 8.8.8.8, google's dns
-# comment out if you don't need
-cookbook_file "/etc/resolv.conf" do
-  source 'resolve.conf'
-  owner  'root'
-  group  'root'
+execute "apt-get update" do
+  user 'root'
 end
 
-%w[wget curl git-core build-essential vim-nox nginx imagemagick].each do |pkg|
+# what is below exists because my dns was messed up in my vagrant box
+# i set the dns to 8.8.8.8, google's dns
+# comment out if you don't need
+#
+# Edit: This has been unnecessary for me lately but
+# I will leave it in until the next release
+#
+#cookbook_file "/etc/resolv.conf" do
+  #source 'resolve.conf'
+  #owner  'root'
+  #group  'root'
+#end
+
+%w[wget curl git-core build-essential vim-nox nginx].each do |pkg|
   package pkg do
     action :install
   end
 end
-
-# this is my default install, the best rvm cookbook i've found is at
-# https://github.com/fnichol/chef-rvm
-# this installs system rvm so at /usr/local/rvm instead of ~/.rvm
-
-# include_recipe "rvm::system"
-
-# execute 'update rubygems' do
-#   user 'root'
-#   command 'gem update --system'
-#   action :run
-# end
 
 cookbook_file "/home/#{node[:user]}/.vimrc" do
   source 'vimrc'
